b3a37ff3258d0596a1956a0e1cc7339e185d2dd3
wkwksama <mailpakgasing@gmail.com>
2018-08-24 20:09:04 +0700

    rancangan route path garis besar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,10 @@
 <template>
   <v-app>
-    <v-toolbar dark fixed app color="primary">
+    <v-toolbar dark fixed dense app color="primary">
       <v-btn
         icon
       >
         <v-icon v-html="'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn
-        icon
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-      >
-        <v-icon>remove</v-icon>
       </v-btn>
       <v-toolbar-title v-text="title"></v-toolbar-title>
       <v-spacer></v-spacer>
@@ -30,10 +20,11 @@
       </v-container>
     </v-content>
     <v-bottom-nav :value="true" :active.sync="activeNavigation" fixed color="white" height="50">
-      <v-btn v-for="(nav, key) in navs" :key="key"  flat color="blue lighten-1" :value="nav.route">
+      <v-btn v-for="(nav, key) in navs" :key="key" :to="nav.path" flat color="blue lighten-1" :value="nav.route">
         <v-icon>{{ nav.icon }}</v-icon>
       </v-btn>
     </v-bottom-nav>
+    <v-card height="36px" class="mt-3" flat color="transparent"></v-card>
   </v-app>
 </template>
 
@@ -46,9 +37,12 @@
           { icon: 'apps', title: 'Welcome', to: '/' },
           { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
         ],
-        activeNavigation: 'history',
+        activeNavigation: 'recent',
         navs: [
-          { route: 'recent', icon: 'history' }
+          { route: 'home', path: '/', icon: 'home' },
+          { route: 'acara', path: '/acara', icon: 'event' },
+          { route: 'pengumuman', path: '/pengumuman', icon: 'history' },
+          { route: 'profile', path: '/profile', icon: 'account_circle' },
         ]
       }
     }
---
+++ pages/pengumuman/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>pengumuman</div>
+</template>
